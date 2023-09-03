e960fd8d9efe17ff297a3dd569a2581fc676d854
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-09-02 23:41:16 -0700

    Componente creado.

--- src/auth/SignUp.js
+++ src/auth/SignUp.js
@@ -1,8 +1,35 @@
+import userIcon from '../images/user-icon.png'
+import passwordIcon from '../images/password-icon.png'
+import emailIcon from '../images/email-icon.png'
+
+import Layout from './Layout';
+import Textfield from '../components/Textfield';
+import { Link } from 'react-router-dom';
+
 function SignUp() {
-    return (
-      <>
-      </>
-    );
-  }
-  
-  export default SignUp;
+  return (
+    <Layout bottom={ <Bottom /> }>
+      <h1 className='mb-3 text-center'>Sign up</h1>
+      <form className='mx-auto'>
+        <Textfield name='username' label='Username' icon={ userIcon } />
+        <Textfield name='email' label='Email' icon={ emailIcon } />
+        <Textfield name='password' label='Password' type='password' icon={ passwordIcon } />
+        <div className='row mx-auto flex-column gap-2'>
+          <button className='btn btn-primary btn-mw mx-auto'>Sign up</button>
+        </div>
+      </form>
+    </Layout>
+  );
+}
+
+function Bottom() {
+  return (
+    <div className='row mt-1'>
+      <Link to='/' className='btn btn-link btn-mw mx-auto'>
+        Already have an account?
+      </Link>
+    </div>
+  )
+}
+
+export default SignUp;
