e9d45b0fa1926e53d6e34967a8d307aa18a66c64
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-09-03 17:58:34 -0700

    Componente creado.

--- src/auth/ForgotPass.js
+++ src/auth/ForgotPass.js
@@ -1,8 +1,31 @@
+import emailIcon from '../images/email-icon.png'
+
+import Layout from './Layout';
+import Textfield from '../components/Textfield';
+import { Link } from 'react-router-dom';
+
 function ForgotPass() {
     return (
-      <>
-      </>
+      <Layout bottom={ <Bottom /> }>
+        <h1 className='mb-3 text-center'>Recover your password</h1>
+        <form className='mx-auto'>
+          <Textfield name='email' label='Email' icon={ emailIcon }/>
+          <div className='row mx-auto flex-column gap-2'>
+            <button className='btn btn-primary btn-mw mx-auto'>Send email</button>
+          </div>
+        </form>
+      </Layout>
     );
   }
   
+  function Bottom() {
+    return (
+      <div className='row mt-1'>
+        <Link to='/' className='btn btn-link btn-mw mx-auto'>
+          Return to login
+        </Link>
+      </div>
+    )
+  }
+
   export default ForgotPass;
