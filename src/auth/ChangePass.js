f6f256677512a48b10f1d90ec2f65ea61c052cbc
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-09-03 17:58:19 -0700

    Componente creado.

--- src/auth/ChangePass.js
+++ src/auth/ChangePass.js
@@ -1,7 +1,21 @@
+import Layout from './Layout';
+import Textfield from '../components/Textfield';
+
+import newPasswordIcon from '../images/new-password-icon.png'
+import repeatPasswordIcon from '../images/repeat-password-icon.png'
+
 function ChangePass() {
     return (
-      <>
-      </>
+      <Layout>
+        <h1 className='mb-3 text-center'>Change your password</h1>
+        <form className='mx-auto'>
+          <Textfield name='newPassword' label='New password' type='password' icon={ newPasswordIcon }/>
+          <Textfield name='repeatPassword' label='Repeat password' type='password' icon={ repeatPasswordIcon }/>
+          <div className='row mx-auto flex-column gap-2'>
+            <button className='btn btn-primary btn-mw mx-auto'>Change password</button>
+          </div>
+        </form>
+      </Layout>
     );
   }
   
